7433ddd2edae4dee52411db06a5638a670c03880
milkcoke <mbh023@gmail.com>
2023-07-28 22:15:59 +0900

    feat: Add createOrder for Separating non-testable code to external

--- src/main/java/kioskapp/CafeKiosk.java
+++ src/main/java/kioskapp/CafeKiosk.java
@@ -5,16 +5,29 @@ import kioskapp.domain.order.Order;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 public class CafeKiosk {
 
+    private static final LocalTime SHOP_OPEN_TIME = LocalTime.of(7, 0);
+    private static final LocalTime SHOP_CLOSE_TIME = LocalTime.of(18, 0);
+
     private final List<Beverage> beverages = new ArrayList<>();
 
     public void add(Beverage beverage) {
         beverages.add(beverage);
+    }
+
+    public void add(Beverage beverage, int count) {
+        if (count <= 0) {
+            throw new IllegalArgumentException("You should order beverage greater than or equal to 1");
+        }
+        for (var i = 0; i < count; i++) {
+            beverages.add(beverage);
+        }
     }
 
     public void remove(Beverage beverage) {
@@ -33,8 +46,28 @@ public class CafeKiosk {
         return totalPrice;
     }
 
+    /*
+    * 테스트 어려운 코드
+    * 랜덤값, 현재 시간 값 등 Input 의 예측 불가능성
+    */
     public Order createOrder() {
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        LocalTime currentTime = currentDateTime.toLocalTime();
+        if (currentTime.isBefore(SHOP_OPEN_TIME) || currentTime.isAfter(SHOP_CLOSE_TIME)) {
+            throw new IllegalArgumentException("You cannot place an order between 07:00 and 18:00");
+        }
         return new Order(LocalDateTime.now(), beverages);
     }
 
+    /*
+    * 테스트 용이한 코드
+    * Input 의 의존성을 외부로 분리하여 예측 가능성.
+    */
+    public Order createOrder(LocalDateTime currentDateTime) {
+        LocalTime currentTime = currentDateTime.toLocalTime();
+        if (currentTime.isBefore(SHOP_OPEN_TIME) || currentTime.isAfter(SHOP_CLOSE_TIME)) {
+            throw new IllegalArgumentException("You cannot place an order between 07:00 and 18:00");
+        }
+        return new Order(LocalDateTime.now(), beverages);
+    }
 }
