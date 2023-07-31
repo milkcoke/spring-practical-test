7648aa036b3e911d96c895e464e81ff556638dba
milkcoke <mbh023@gmail.com>
2023-07-31 14:50:40 +0900

    build: Add spring jpa dependency

--- src/main/java/kioskapp/CafeKioskApplication.java
+++ src/main/java/kioskapp/CafeKioskApplication.java
@@ -1,8 +1,10 @@
 package kioskapp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class CafeKioskApplication {
 
