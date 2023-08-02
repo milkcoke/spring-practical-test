f0aac1993adee2ea6286107a000347883ccad23f
milkcoke <mbh023@gmail.com>
2023-08-02 23:27:54 +0900

    test: Add Order unit test

---
+++ src/test/java/kioskapp/domain/order/OrderTest.java
@@ -0,0 +1,69 @@
+package kioskapp.domain.order;
+
+import kioskapp.domain.product.Product;
+import kioskapp.respository.order.OrderRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class OrderTest {
+    @Autowired
+    private OrderRepository orderRepository;
+    Product americano = Product.builder()
+            .name("Americano")
+            .price(4000)
+            .build();
+
+    Product latte = Product.builder()
+            .name("latte")
+            .price(4500)
+            .build();
+
+    Product pineappleBread = Product.builder()
+            .name("소보로빵")
+            .price(1500)
+            .build();
+    @Test
+    @DisplayName("주문 시점에 주문 총 금액 표기")
+    void calculateTotalPrice() {
+        // given
+        var products = List.of(americano, latte, pineappleBread);
+
+        // when
+        Order order = new Order(products);
+
+        // then
+        assertThat(order.getTotalPrice()).isEqualTo(10_000);
+    }
+
+    @Test
+    @DisplayName("최초 주문시 상태는 INIT 이다.")
+    public void initStatus() {
+        // given
+        var products = List.of(americano, latte, pineappleBread);
+        // when
+        Order order = new Order(products);
+        // then
+        assertThat(order.getOrderStatus()).isEqualTo(OrderStatus.INIT);
+    }
+
+    @Test
+    public void checkCreatedTime() {
+        // given
+        var products = List.of(americano, latte, pineappleBread);
+        Order order = new Order(products);
+        // when
+        this.orderRepository.save(order);
+        // then
+        assertThat(order.getCreateDateTime())
+                .isNotNull()
+                .isBeforeOrEqualTo(LocalDateTime.now());
+    }
+}
