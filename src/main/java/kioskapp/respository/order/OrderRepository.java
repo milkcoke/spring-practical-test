383f1812e7ab34a6768e51e21835b2b2445f9bd6
milkcoke <mbh023@gmail.com>
2023-08-02 23:26:21 +0900

    feat: Add OrderRepository

---
+++ src/main/java/kioskapp/respository/order/OrderRepository.java
@@ -0,0 +1,9 @@
+package kioskapp.respository.order;
+
+import kioskapp.domain.order.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
