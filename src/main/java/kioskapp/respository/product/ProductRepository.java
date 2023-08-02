a9bfee89ef3fce068131644e9035db0b495e4b66
milkcoke <mbh023@gmail.com>
2023-08-02 18:18:42 +0900

    feat: Add find all products by product numbers method on ProductRepository

--- src/main/java/kioskapp/respository/product/ProductRepository.java
+++ src/main/java/kioskapp/respository/product/ProductRepository.java
@@ -16,4 +16,5 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
      */
     List<Product> findAllBySellingStatusIn(List<ProductSellingStatus> sellingStatuses);
     List<Product> findAllBySellingStatusIs(ProductSellingStatus sellingStatus);
+    List<Product> findAllByProductNumberIn(List<String> productNumbers);
 }
