ef9cbbb239059b592d4f81a83f265c9f83a09d5b
milkcoke <mbh023@gmail.com>
2023-08-02 18:17:05 +0900

    feat: Define Order entity

--- src/main/java/kioskapp/domain/order/Order.java
+++ src/main/java/kioskapp/domain/order/Order.java
@@ -1,15 +1,30 @@
 package kioskapp.domain.order;
 
-import kioskapp.domain.beverage.Beverage;
+import jakarta.persistence.*;
+import kioskapp.domain.BaseEntity;
+import kioskapp.domain.orderproduct.OrderProduct;
+import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-import java.time.LocalDateTime;
+import lombok.NoArgsConstructor;
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
-@RequiredArgsConstructor
-public class Order {
-    private final LocalDateTime orderDateTime;
-    private final List<Beverage> beverages;
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "orders")
+public class Order extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    private int totalPrice;
+
+    // FIXME: 연관관계 주인 다시 살펴보기
+    // 양방향 연관관계 주인 == 외래키가 있는 객체
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderProduct> orderProducts = new ArrayList<>();
 }
