2bb5cec2a342d60288d983aa36c8ad2ecaf25e78
milkcoke <mbh023@gmail.com>
2023-08-02 23:28:41 +0900

    feat: Update OrderService
    Implement OrderService with OrderCreateResponse and Product.

--- src/main/java/kioskapp/service/order/OrderService.java
+++ src/main/java/kioskapp/service/order/OrderService.java
@@ -1,6 +1,8 @@
 package kioskapp.service.order;
 
+import kioskapp.domain.order.Order;
 import kioskapp.domain.product.Product;
+import kioskapp.respository.order.OrderRepository;
 import kioskapp.respository.product.ProductRepository;
 import kioskapp.service.order.dto.OrderCreateRequest;
 import kioskapp.service.order.dto.OrderCreateResponse;
@@ -13,9 +15,14 @@ import java.util.List;
 @RequiredArgsConstructor
 public class OrderService {
     private final ProductRepository productRepository;
+    private final OrderRepository orderRepository;
+
     public OrderCreateResponse createOrder(OrderCreateRequest orderCreateRequest) {
         List<String> productNumbers = orderCreateRequest.getProductNumbers();
         List<Product> products = this.productRepository.findAllByProductNumberIn(productNumbers);
-        // TODO: Implement Order service with order and products
+        Order order = new Order(products);
+        this.orderRepository.save(order);
+
+        return OrderCreateResponse.of(order);
     }
 }
