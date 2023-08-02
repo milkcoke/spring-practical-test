25e083b822be4148b684866fe37227d708c33f68
milkcoke <mbh023@gmail.com>
2023-08-02 18:19:35 +0900

    feat: Define OrderProduct Entity

---
+++ src/main/java/kioskapp/domain/orderproduct/OrderProduct.java
@@ -0,0 +1,29 @@
+package kioskapp.domain.orderproduct;
+
+import jakarta.persistence.*;
+import kioskapp.domain.BaseEntity;
+import kioskapp.domain.order.Order;
+import kioskapp.domain.product.Product;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class OrderProduct extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Product product;
+
+    public OrderProduct(Order order, Product product) {
+        this.order = order;
+        this.product = product;
+    }
+}
