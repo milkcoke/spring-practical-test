6e70a3756094f42b85cd995a108a92dfb5a30c0a
milkcoke <mbh023@gmail.com>
2023-08-03 10:36:24 +0900

    refactor: Add @NoArgsConstructor annotation on OrderCreateRequest

--- src/main/java/kioskapp/service/order/dto/OrderCreateRequest.java
+++ src/main/java/kioskapp/service/order/dto/OrderCreateRequest.java
@@ -2,11 +2,14 @@ package kioskapp.service.order.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
 public class OrderCreateRequest {
+    // 오로지 상품 번호로만 주문한다고 가정.
     private List<String> productNumbers;
 
     @Builder
