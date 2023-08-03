3931f35d8fb5a6d6b61e3a62bb4390fe55556881
milkcoke <mbh023@gmail.com>
2023-08-03 10:36:06 +0900

    test: Add unit test about duplicate product numbers order

--- src/test/java/kioskapp/service/order/OrderServiceTest.java
+++ src/test/java/kioskapp/service/order/OrderServiceTest.java
@@ -2,9 +2,11 @@ package kioskapp.service.order;
 
 import kioskapp.domain.product.Product;
 import kioskapp.domain.product.ProductType;
+import kioskapp.respository.orderproduct.OrderProductRepository;
 import kioskapp.respository.product.ProductRepository;
 import kioskapp.service.order.dto.OrderCreateRequest;
 import kioskapp.service.order.dto.OrderCreateResponse;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,14 +28,22 @@ class OrderServiceTest {
     private OrderService orderService;
     @Autowired
     private ProductRepository productRepository;
+    @Autowired
+    private OrderProductRepository orderProductRepository;
+
+    @AfterEach
+    void clean() {
+        this.orderProductRepository.deleteAllInBatch();
+        this.productRepository.deleteAllInBatch();
+    }
+    Product americano = createProduct(HANDMADE, "001", "아메리카노", 4000);
+    Product cafeLatte = createProduct(HANDMADE, "002", "카페라떼", 4500);
+    Product pineappleBread = createProduct(BAKERY, "003", "소보로빵", 1500);
 
     @Test
     @DisplayName("주문 번호 리스트를 받아 주문 생성")
     public void createOrder() {
       // given
-      Product americano = createProduct(HANDMADE, "001", "아메리카노", 4000);
-      Product cafeLatte = createProduct(HANDMADE, "002", "카페라떼", 4500);
-      Product pineappleBread = createProduct(BAKERY, "003", "소보로빵", 1500);
       productRepository.saveAll(List.of(americano, cafeLatte, pineappleBread));
       OrderCreateRequest orderCreateRequest = OrderCreateRequest.builder()
               .productNumbers(List.of("001", "002"))
@@ -49,6 +59,26 @@ class OrderServiceTest {
               .containsExactlyInAnyOrder(
                       tuple("001", "아메리카노", 4000),
                       tuple("002", "카페라떼", 4500)
+              );
+    }
+
+    @Test
+    @DisplayName("중복 상품 여러개 주문 가능")
+    public void orderDuplicateProductNumbers() {
+      // given
+      productRepository.saveAll(List.of(americano, cafeLatte, pineappleBread));
+      OrderCreateRequest orderRequest = OrderCreateRequest.builder()
+                .productNumbers(List.of("001", "001"))
+                .build();
+
+      // when
+      OrderCreateResponse orderCreateResponse = orderService.createOrder(orderRequest);
+      // then
+      assertThat(orderCreateResponse.getProductResponses()).hasSize(2)
+              .extracting("name", "price")
+              .containsExactlyInAnyOrder(
+                      tuple("아메리카노", 4000),
+                      tuple("아메리카노", 4000)
               );
     }
 
