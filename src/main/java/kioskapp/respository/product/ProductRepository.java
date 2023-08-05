f6555bb11609326c17034ffd8485e6e600513e08
milkcoke <mbh023@gmail.com>
2023-08-05 23:54:54 +0900

    feat: Add findLatestProductNumber method on ProductRepository

--- src/main/java/kioskapp/respository/product/ProductRepository.java
+++ src/main/java/kioskapp/respository/product/ProductRepository.java
@@ -3,6 +3,7 @@ package kioskapp.respository.product;
 import kioskapp.domain.product.Product;
 import kioskapp.domain.product.ProductSellingStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,4 +18,10 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findAllBySellingStatusIn(List<ProductSellingStatus> sellingStatuses);
     List<Product> findAllBySellingStatusIs(ProductSellingStatus sellingStatus);
     List<Product> findAllByProductNumberIn(List<String> productNumbers);
+
+    // Query DSL 의 Builder 패턴을 사용했든
+    // Native Query 를 직접 정의했든
+    // Spring Data JPA 의 쿼리 메소드를 썼든 테스트 쿼리는 필요하다.
+    @Query(value = "SELECT p.product_number FROM product p ORDER BY id desc limit 1", nativeQuery = true)
+    String findLatestProductNumber();
 }
